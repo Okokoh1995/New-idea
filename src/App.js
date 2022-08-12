2f6971f28fcc3af6dc736d7a257a82429b7353fc
okokoh1995 <mondaycokokoh@gmail.con>
2022-08-12 11:57:21 +0100

    add item item schedule

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
 import './App.css';
+import "./style.css";
 
 function App() {
+  // const [isImportant, setIsImportant] = React.useState("Yes")
+  // function handleClick() {
+  //   setIsImportant("No")
+  // }
+
+  // const [count, setCount] = React.useState(0)
+
+  // function add() {
+  //   setCount(count + 1)
+  // }
+
+  // function subtract() {
+  //   setCount(count - 1)
+  // }
+
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+
+  function addItem() {
+    setThingsArray(prevThingsArray => {
+        return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]
+    })
+    
+
+  }
+
+  const thingsElement = thingsArray.map(Thing => <p key={Thing}>{Thing}</p>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={addItem}>Add Item</button>
+       {thingsElement}
     </div>
+
+
+    // <div className="state">
+    //   <h1 className="state--title">Is state important to know?</h1>
+    //   <div className="state--value" onClick={handleClick}>
+    //     <h1>{isImportant}</h1>
+    //   </div>
+    // </div>
+
+    // <div className="counter">
+    //   <button className="counter--minus" onClick={subtract}>-</button>
+    //   <div className="counter--count">
+    //     <h1>{count}</h1>
+    //   </div>
+    //   <button className="counter--plus"  onClick={add}>+</button>
+    // </div>
   );
 }
 
